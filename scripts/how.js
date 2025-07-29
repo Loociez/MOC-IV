49f690347401479197212abd356cbc3f1d6e2461
Loociez <loocielambert@gmail.com>
2025-07-29 12:54:51 +0100

    update
    how to and index update

---
+++ scripts/how.js
@@ -0,0 +1,12 @@
+{
+  "title": "📖 How to Use & Uninstall Scripts",
+  "steps": [
+    "If you are using a Chromium browser to play the game (Edge, Chrome, Opera and so on)",
+    "Right-click the chat and press \"Inspect Element\".",
+    "Click on the tab called \"Console\" — this is where you will paste the code.",
+    "Paste the code you copied from the script above.",
+    "Once you’ve submitted/entered the code, you can close the console.",
+    "",
+    "To uninstall: Simply refresh your browser — this is a temporary change."
+  ]
+}
