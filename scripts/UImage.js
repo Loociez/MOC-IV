f1623697bb799f62f72fa4e53bc6fbcdd6871b89
Loociez <loocielambert@gmail.com>
2025-09-12 13:35:35 +0100

    Update UImage.js

--- scripts/UImage.js
+++ scripts/UImage.js
@@ -1,48 +1,19 @@
 // imageReplacer.js
 (function () {
   // Define a mapping of original images to the new images
-  const imageReplacementMap = [
-    {
-      match: img => img.src.includes('header.png'),
-      newSrc: chrome.runtime.getURL('images/header.png')
-    },
-    {
-      match: img => img.src.includes('stats1.png'),
-      newSrc: chrome.runtime.getURL('images/stats1.png')
-    },
-    {
-      match: img => img.src.includes('dungeons1.png'),
-      newSrc: chrome.runtime.getURL('images/dungeons1.png')
-    },
-    {
-      match: img => img.src.includes('guilds1.png'),
-      newSrc: chrome.runtime.getURL('images/guilds1.png')
-    },
-    {
-      match: img => img.src.includes('mobile1.png'),
-      newSrc: chrome.runtime.getURL('images/mobile1.png')
-    },
-    {
-      match: img => img.src.includes('quit1.png'),
-      newSrc: chrome.runtime.getURL('images/quit1.png')
-    },
-    {
-      match: img => img.src.includes('settings1.png'),
-      newSrc: chrome.runtime.getURL('images/settings1.png')
-    },
-    {
-      match: img => img.src.includes('shop1.png'),
-      newSrc: chrome.runtime.getURL('images/shop1.png')
-    },
-    {
-      match: img => img.src.includes('trade1.png'),
-      newSrc: chrome.runtime.getURL('images/trade1.png')
-    },
-    {
-      match: img => img.src.includes('serpdrag.png'),
-      newSrc: chrome.runtime.getURL('images/serpdrag.png')
-    }
-  ];
+const imageReplacementMap = [
+  { match: img => img.src.includes('header.png'), newSrc: 'https://loociez.github.io/MOC-IV/images/header.png' },
+  { match: img => img.src.includes('stats1.png'), newSrc: 'https://loociez.github.io/MOC-IV/images/stats1.png' },
+  { match: img => img.src.includes('dungeons1.png'), newSrc: 'https://loociez.github.io/MOC-IV/images/dungeons1.png' },
+  { match: img => img.src.includes('guilds1.png'), newSrc: 'https://loociez.github.io/MOC-IV/images/guilds1.png' },
+  { match: img => img.src.includes('mobile1.png'), newSrc: 'https://loociez.github.io/MOC-IV/images/mobile1.png' },
+  { match: img => img.src.includes('quit1.png'), newSrc: 'https://loociez.github.io/MOC-IV/images/quit1.png' },
+  { match: img => img.src.includes('settings1.png'), newSrc: 'https://loociez.github.io/MOC-IV/images/settings1.png' },
+  { match: img => img.src.includes('shop1.png'), newSrc: 'https://loociez.github.io/MOC-IV/images/shop1.png' },
+  { match: img => img.src.includes('trade1.png'), newSrc: 'https://loociez.github.io/MOC-IV/images/trade1.png' },
+  { match: img => img.src.includes('serpdrag.png'), newSrc: 'https://loociez.github.io/MOC-IV/images/serpdrag.png' }
+];
+
 
   function replaceImages() {
     document.querySelectorAll('img').forEach(img => {
