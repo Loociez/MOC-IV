dae80f8ad851056450752e743fe910eb56b27adc
Loociez <loocielambert@gmail.com>
2025-12-26 14:09:01 +0000

    up

--- scripts/UImage.js
+++ scripts/UImage.js
@@ -6,6 +6,7 @@
     { match: img => img.src.includes('header.png'), newSrc: 'https://loociez.github.io/MOC-IV/images/header.png' },
     { match: img => img.src.includes('stats1.png'), newSrc: 'https://loociez.github.io/MOC-IV/images/stats1.png' },
     { match: img => img.src.includes('dungeons1.png'), newSrc: 'https://loociez.github.io/MOC-IV/images/dungeons1.png' },
+    { match: img => img.src.includes('leaderboard1.png'), newSrc: 'https://loociez.github.io/MOC-IV/images/leaderboard1.png' },
     { match: img => img.src.includes('guilds1.png'), newSrc: 'https://loociez.github.io/MOC-IV/images/guilds1.png' },
     { match: img => img.src.includes('mobile1.png'), newSrc: 'https://loociez.github.io/MOC-IV/images/mobile1.png' },
     { match: img => img.src.includes('quit1.png'), newSrc: 'https://loociez.github.io/MOC-IV/images/quit1.png' },
